700867b762c44018b5250c3f1a3384da2f823e61
aphu <alexp6114@gmail.com>
2023-07-03 12:15:12 -0400

    fix

--- vim/setup.sh
+++ vim/setup.sh
@@ -7,4 +7,4 @@ vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 
 # Color Scheme
 # https://github.com/joshdick/onedark.vim
-git clone git@github.com:joshdick/onedark.vim.git ~/.vim/pack/onedark/opt/
+git clone git@github.com:joshdick/onedark.vim.git ~/.vim/pack/onedark/opt/onedark.vim
