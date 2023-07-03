31999a9b95063fd96222e5e96de67e3c07f2a4d0
aphu <alexp6114@gmail.com>
2023-07-03 12:07:02 -0400

    vim onedark color scheme

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 # NerdTree
-git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+# https://github.com/preservim/nerdtree
+git clone git@github.com:preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
+# Color Scheme
+# https://github.com/joshdick/onedark.vim
+git clone git@github.com:joshdick/onedark.vim.git ~/.vim/pack/onedark/opt/
