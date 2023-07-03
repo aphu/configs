2b846d36370d1666739da43fb9ed9f7266d77670
aphu <alexp6114@gmail.com>
2023-07-03 12:39:20 -0400

    add kotlin syntax highlighting

--- vim/setup.sh
+++ vim/setup.sh
@@ -8,3 +8,7 @@ vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 # Color Scheme
 # https://github.com/joshdick/onedark.vim
 git clone git@github.com:joshdick/onedark.vim.git ~/.vim/pack/onedark/opt/onedark.vim
+
+# Kotlin Syntax Highlighting
+# https://github.com/udalov/kotlin-vim
+git clone https://github.com/udalov/kotlin-vim.git ~/.vim/pack/plugins/start/kotlin-vim
