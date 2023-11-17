df0c062eb12a1271fe6c770ee1b3a9a6d569d26b
aphu <alex7964@protonmail.com>
2023-11-17 16:52:08 -0500

    update kitty theme and vim

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-# NerdTree
-# https://github.com/preservim/nerdtree
-git clone git@github.com:preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
-
-# Color Scheme
-# https://github.com/joshdick/onedark.vim
-git clone git@github.com:joshdick/onedark.vim.git ~/.vim/pack/onedark/opt/onedark.vim
-
-# Kotlin Syntax Highlighting
-# https://github.com/udalov/kotlin-vim
-git clone https://github.com/udalov/kotlin-vim.git ~/.vim/pack/plugins/start/kotlin-vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
